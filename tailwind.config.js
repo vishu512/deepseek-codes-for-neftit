7432bac28924a065b6fd3fcb32de537b730a11f4
vishu512 <70514375+vishu512@users.noreply.github.com>
2025-02-06 00:42:53 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E4E4E5",
+        accent: "#39FF14",
+        dark1: "#3C3C3C",
+        dark2: "#0A1F3D"
+      }
+    },
+  },
+  plugins: [],
+}
